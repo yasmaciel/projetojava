9e2ec4bcb5e94ed425ffd1008de384cfe1f5b1c9
Yasmin maciel <yasmin_vm_silva@estudante.sc.senai.br>
2021-06-14 16:42:32 -0300

    Refatoração de classe

--- src/br/com/senai/ProgramaPrincipal.java
+++ src/br/com/senai/ProgramaPrincipal.java
@@ -1,15 +1,22 @@
-package br.com.senai.pessoa;
+package br.com.senai;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import br.com.senai.pessoa.Pessoa;
+import br.com.senai.pessoa.PessoaController;
+import br.com.senai.produto.Produto;
+import br.com.senai.produto.ProdutoController;
 
 public class ProgramaPrincipal {
 
 	public static void main(String[] args) {
 		List<Pessoa> pessoas = new ArrayList<>();
+		List<Produto> produto = new ArrayList<>();
 
 		PessoaController pessoaController = new PessoaController();
-
+		ProdutoController produtoController = new ProdutoController();
+ 
 		boolean sair = false;
 
 		do{
@@ -25,7 +32,16 @@ public class ProgramaPrincipal {
 				case 2:
 					pessoaController.listarPessoas(pessoas);
 					break;
-
+					
+				case 3:
+					produto.add(produtoController.cadastrarProduto());
+					break;
+					
+				case 4:
+					produtoController.listarProdutos(produto);
+					break;
+					
+					
 				case 9:
 					sair = true;
 					break;
